5f25340211f982379e276faba47fed3365767012
GarvSaxena <messagegarv32@gmail.com>
2026-07-22 17:54:34 +0530

    Fix: configured npm workspaces, fixed redis compatibility

--- deploy-service/src/index.ts
+++ deploy-service/src/index.ts
@@ -1,13 +1,12 @@
-import {createClient, commandOptions} from "redis"
-const subscriber = createClient();
-async function main() {
-  while (1) { // infiinitly run this, to pull data from local redis
-    const response = await subscriber.brPop(
-      commandOptions({ isolated: true }),
-      "build-queue",
-      0
-    );
+import { createClient } from "redis";
 
+const subscriber = createClient();
+
+async function main() {
+  await subscriber.connect();
+
+  while (true) {
+    const response = await subscriber.brPop("build-queue", 0);
     console.log(response);
   }
 }
